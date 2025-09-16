db800608c7ecc2d7b37f40dd08bee618981324bc
JogadorNumeroUm <nickolaslamin@gmail.com>
2025-09-15 22:01:32 -0300

    Add Custo, Categoria, and Preparo fields to Receita class constructor

--- TudoGostoso/src/TudoGostoso/Receita.java
+++ TudoGostoso/src/TudoGostoso/Receita.java
@@ -6,11 +6,19 @@ public class Receita {
     String descricao;
     String imagem;
 
-    public Receita(int IdReceita, String titulo, String descricao, String imagem){
+    
+    Custo custo;
+    Categoria categoria;
+    Preparo preparo;
+
+    public Receita(int IdReceita, String titulo, String descricao, String imagem,Custo custo, Categoria categoria, Preparo preparo){
         this.IdReceita = IdReceita;
         this.titulo = titulo;
         this.descricao = descricao;
         this.imagem = imagem;
+        this.custo = custo;
+        this.categoria = categoria;
+        this.preparo = preparo;
     }
 
 }
