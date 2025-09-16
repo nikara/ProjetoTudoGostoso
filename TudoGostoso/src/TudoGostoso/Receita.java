66ee6330f9d7903bfa02b1b5216ec4250ee882d1
JogadorNumeroUm <nickolaslamin@gmail.com>
2025-09-15 21:50:21 -0300

    Add Receita, Categoria, Custo, and Preparo classes for recipe management

--- TudoGostoso/src/TudoGostoso/Receita.java
+++ TudoGostoso/src/TudoGostoso/Receita.java
@@ -1,5 +1,16 @@
 package TudoGostoso;
 
 public class Receita {
-    
+    int IdReceita;
+    String titulo;
+    String descricao;
+    String imagem;
+
+    public Receita(int IdReceita, String titulo, String descricao, String imagem){
+        this.IdReceita = IdReceita;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.imagem = imagem;
+    }
+
 }
