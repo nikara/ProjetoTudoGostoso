aa45ba1a13cb0830b04814052dc9539b2d9dda10
JogadorNumeroUm <nickolaslamin@gmail.com>
2025-09-22 22:00:06 -0300

    refactor: enhance menu options display and instantiate Categoria object

--- TudoGostoso/src/TudoGostoso/Sistema.java
+++ TudoGostoso/src/TudoGostoso/Sistema.java
@@ -31,23 +31,43 @@ public class Sistema {
 
             switch (i) {
                 case 1:
+                System.out.println(" 1 - Adicionar Categoria ");
+                System.out.println(" 2 - Listar Categoria ");
+                    int x = sc.nextInt();
+                    if(x == 1){
+                        
+                    }else if (x == 2){
+
+                    }
+
+
                     
                     break;
                 case 2:
+
+                System.out.println(" 1 - Adicionar Custo ");
+                System.out.println(" 2 - Listar Custo ");
                     
                     break;
                 case 3:
+                System.out.println(" 1 - Adicionar Preparo ");
+                System.out.println(" 2 - Listar Preparo ");
                     
                     break;
                 case 4:
+                System.out.println(" 1 - Adicionar Utensilio ");
+                System.out.println(" 2 - Listar Utensilio ");
                     
                     break;
                 case 5:
+                System.out.println(" 1 - Adicionar Receita ");
+                System.out.println(" 2 - Listar Receita ");
                     
                     break;
                 case 6:
+
                     chave = 'C';
-                    
+
                     break;
             
                 default:
