6167b92d13df48849a5109d7678e4ca7dfc98c15
JogadorNumeroUm <nickolaslamin@gmail.com>
2025-09-22 21:29:55 -0300

    refactor: improve menu structure and user input handling in Sistema

--- TudoGostoso/src/TudoGostoso/Sistema.java
+++ TudoGostoso/src/TudoGostoso/Sistema.java
@@ -1,5 +1,8 @@
 package TudoGostoso;
 
+import java.lang.classfile.instruction.SwitchCase;
+import java.util.Scanner;
+
 public class Sistema {
     public static void main(String[] args) {
 
@@ -10,7 +13,48 @@ public class Sistema {
         // Finaliza o programa
         char chave = 'X';
 
-        while (true ) {
+        Scanner sc = new Scanner(System.in);
+
+        while (chave == 'X' ) {
+
+            int opcao;
+
+            System.out.println("Escolha uma opção ");
+            System.out.println(" 1 - Caterigoria ");
+            System.out.println(" 2 - Custo ");
+            System.out.println(" 3 - Preparo ");
+            System.out.println(" 4 - Utensilio ");
+            System.out.println(" 5 - Receita ");
+            System.out.println(" 6 - Fechar o Programa ");
+            
+            int i = sc.nextInt();
+
+            switch (i) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:
+                    
+                    break;
+                case 6:
+                    chave = 'C';
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+
+            
             
         }
         
