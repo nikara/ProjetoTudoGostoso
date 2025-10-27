9accdb26ec5ad6e2102e01687186915c0f7fc23a
JogadorNumeroUm <nickolaslamin@gmail.com>
2025-10-27 20:16:29 -0300

    feat: implement HTTP server and define route for Administrador

---
+++ TudoGostoso/src/TudoGostoso/app.java
@@ -0,0 +1,26 @@
+package TudoGostoso;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import TudoGostoso.model.*;
+
+public class app {
+
+    public static void main(String[] args) throws IOException {
+        // Criando o servidor HTTP
+        HttpServer server = HttpServer.create(new InetSocketAddress(8089), 0);
+
+        // Definindo as rotas
+        server.createContext("/Administrador", new Administrador());
+        
+
+        // Iniciando o servidor
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Servidor rodando em http://localhost:8089/");
+    }
+    
+}
