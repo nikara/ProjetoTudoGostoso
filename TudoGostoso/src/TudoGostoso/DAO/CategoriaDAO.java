02d39d1f2ae2c7dbaca31e924b513c7d5f937814
JogadorNumeroUm <nickolaslamin@gmail.com>
2025-11-17 19:41:14 -0300

    Refactor CategoriaDAO methods for consistency and add Preparo class skeleton

--- TudoGostoso/src/TudoGostoso/DAO/CategoriaDAO.java
+++ TudoGostoso/src/TudoGostoso/DAO/CategoriaDAO.java
@@ -10,35 +10,33 @@ import java.util.List;
 import com.mysql.cj.x.protobuf.MysqlxPrepare.Prepare;
 import java.sql.SQLException;
 
-
-
 public class CategoriaDAO {
-    
-    public void inserirCategoria(Categoria categoria)throws Exception{
+
+    public void inserirCategoria(Categoria categoria) throws Exception {
         Connection connection = DAO.createConnection();
 
-        try{
-        PreparedStatement stmt = connection.prepareStatement("INSERT into categoria (categoria,status) VALUE (?,?);");
-        stmt.setString(1, categoria.getCategoria());
-        stmt.setBoolean(2, categoria.getStatus());
+        try {
+            PreparedStatement stmt = connection
+                    .prepareStatement("INSERT into categoria (categoria,status) VALUE (?,?);");
+            stmt.setString(1, categoria.getCategoria());
+            stmt.setBoolean(2, categoria.getStatus());
 
-        int verifica = stmt.executeUpdate();
-        
-        if( verifica == 0 ){
-            throw new Exception("Nenhuma categoria inserida");
-        }
+            int verifica = stmt.executeUpdate();
 
-        
-        stmt.close();
-        DAO.closeConnection();
+            if (verifica == 0) {
+                throw new Exception("Nenhuma categoria inserida");
+            }
 
-        }catch(SQLException e){
-            throw new Exception ("Erro ao inserir categoria " + e.getMessage(), e);
+            stmt.close();
+            DAO.closeConnection();
+
+        } catch (SQLException e) {
+            throw new Exception("Erro ao inserir categoria " + e.getMessage(), e);
         }
 
     }
 
-    public void deletarCategoria(Categoria categoria)throws Exception{
+    public void deletarCategoria(Categoria categoria) throws Exception {
         Connection connection = DAO.createConnection();
 
         try {
@@ -46,43 +44,83 @@ public class CategoriaDAO {
             stmt.setInt(1, categoria.getIdCategoria());
 
             int verifica = stmt.executeUpdate();
-            if( verifica == 0){
-                throw new  Exception( "Nenhuma categoria deletada");
+            if (verifica == 0) {
+                throw new Exception("Nenhuma categoria deletada");
             }
 
             stmt.close();
             DAO.closeConnection();
 
         } catch (SQLException e) {
-            throw new Exception ("Erro ao deletar Categoria " + e.getMessage(), e);
+            throw new Exception("Erro ao deletar Categoria " + e.getMessage(), e);
         }
     }
 
-    public void AtualizarCategoria(Categoria categoria)throws Exception{
+    public void AtualizarCategoria(Categoria categoria) throws Exception {
         Connection connection = DAO.createConnection();
 
         try {
-            PreparedStatement stmt = connection.prepareStatement("UPDATE categoria SET categoria = ?, status = ? WHERE idCategoria=?");
+            PreparedStatement stmt = connection
+                    .prepareStatement("UPDATE categoria SET categoria = ?, status = ? WHERE idCategoria=?");
             stmt.setString(1, categoria.getCategoria());
             stmt.setBoolean(2, categoria.getStatus());
             stmt.setInt(3, categoria.getIdCategoria());
 
             int verifica = stmt.executeUpdate();
-            if( verifica == 0){
-                throw new  Exception( "Nenhuma categoria atualizada");
+            if (verifica == 0) {
+                throw new Exception("Nenhuma categoria atualizada");
             }
 
             stmt.close();
             DAO.closeConnection();
 
-        }catch (SQLException e) {
-            throw  new Exception("Erro ao atualizar Categoria "+ e.getMessage(), e);
+        } catch (SQLException e) {
+            throw new Exception("Erro ao atualizar Categoria " + e.getMessage(), e);
         }
     }
 
-    public void buscarPorId(int idCategoria)throws Ex
+    public Categoria buscarPorId(int idCategoria)throws Exception{
+        Connection connection = DAO.createConnection();
+        try{
+            PreparedStatement stmt = connection.prepareStatement("SELECT * FROM categoria idReceita = ?");
+            stmt.setInt(1, idCategoria);
+            ResultSet rs = stmt.executeQuery();
+            Categoria categoria = null;
+            if(rs.next()){
+                categoria = new Categoria(rs.getString("categoria"),rs.getBoolean("status"));
+                categoria.setIdCategoria(rs.getInt("idCategoria"));
+            }
+            rs.close();
+            stmt.close();
+            DAO.closeConnection();
+            return categoria;
+        }catch(SQLException e){
+            throw new Exception("Erro ao buscar id "+e.getMessage(), e);
+        }
+    }
 
+    public List<Categoria> listarCategoria() throws Exception{
+        Connection connection = DAO.createConnection();
 
+        try{
+        ResultSet rs = connection.createStatement().executeQuery("SELECT * FROM categoria;");
+        List<Categoria> lista = new ArrayList<>();
 
+        while (rs.next()) {
+            Categoria categoria = new Categoria(
+                rs.getString("categoria"),
+                rs.getBoolean("status")
+            ); 
+            categoria.setIdCategoria(rs.getInt("idReceita"));
+            lista.add(categoria);
+        }
+        rs.close();
+        DAO.closeConnection();
+        return lista;
+        
+        }catch(SQLException e){
+            throw new Exception("Erro ao listar tabela "+e.getMessage(),e);
+        }
+    }
 
 }
