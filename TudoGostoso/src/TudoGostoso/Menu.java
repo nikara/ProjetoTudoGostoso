823438927fc250966c535c8f8f69a0c923cf0411
JogadorNumeroUm <nickolaslamin@gmail.com>
2025-11-17 22:12:47 -0300

    Add HTTP server setup in Menu class and create API directory

--- TudoGostoso/src/TudoGostoso/Menu.java
+++ TudoGostoso/src/TudoGostoso/Menu.java
@@ -1,5 +1,9 @@
 package TudoGostoso;
 
+
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.ConnectException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -10,7 +14,18 @@ import TudoGostoso.model.*;
 
 
 public class Menu { 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
+        
+        //Criando o servidor HTTP
+        HttpServer server = HttpServer.create(new InetSocketAddress(8089),0);
+
+        //Definindo as rotas
+
+        //Iniciando o servidor
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Servidor rodando em http://localhost:8089/");
+        
         try{
             Connection conexao = DAO.createConnection();
 
